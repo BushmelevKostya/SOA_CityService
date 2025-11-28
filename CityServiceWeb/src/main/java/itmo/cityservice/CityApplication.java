ccfd12c878da034df755de89cf5aecda4a52dc1f
kbushmelev <kostikbushmelev@gmail.com>
2025-11-28 10:12:23 +0300

    consul up

--- CityServiceWeb/src/main/java/itmo/cityservice/CityApplication.java
+++ CityServiceWeb/src/main/java/itmo/cityservice/CityApplication.java
@@ -1,8 +1,20 @@
 package itmo.cityservice;
 
+import itmo.cityservice.controller.CityController;
+import itmo.cityservice.cors.CorsFilter;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @ApplicationPath("/api/v1")
 public class CityApplication extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(CityController.class);
+        classes.add(CorsFilter.class);
+        return classes;
+    }
 }
